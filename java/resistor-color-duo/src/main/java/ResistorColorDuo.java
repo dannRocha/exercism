7b713fed8955c93badca5eb0b66c3c1ca21db9e1
Daniel Rocha <rochadaniel15@gmail.com>
2020-09-08 14:59:13 -0300

    solution: resitor-color-duo

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+
 class ResistorColorDuo {
-    int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private final HashMap<String, String> colorScheme = new HashMap<String, String>();
+    private final String[] colors = {"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+
+    ResistorColorDuo(){
+        fillColorScheme();
     }
+
+    private void fillColorScheme(){
+        for(int i = 0; i < colors.length; i++)
+            colorScheme.put(colors[i], String.valueOf(i));	 
+    }
+    
+    int value(String[] colors) {
+
+        String concat = "";
+        final int MAX = 2;
+        
+        for(int i = 0; i < MAX; i++){
+           concat += colorScheme.get(colors[i]);
+        }
+
+        return Integer.valueOf(concat);
+    }
+    
 }
