f2fa2d7bf2acd744fa122e2337e12a0338ec7082
Daniel Rocha <rochadaniel15@gmail.com>
2020-09-08 14:09:16 -0300

    solution: resitor-color

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,23 @@
+import java.util.HashMap;
+
 class ResistorColor {
+    private final HashMap<String, Integer> colorScheme = new HashMap<String, Integer>();
+    private final String[] _colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    ResistorColor(){
+        fillColorScheme();
+    }
+
+    private void fillColorScheme(){
+        for(int i = 0; i < _colors.length; i++)
+            colorScheme.put(_colors[i], i);
+    }
+
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return colorScheme.get(color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return _colors;
     }
 }
